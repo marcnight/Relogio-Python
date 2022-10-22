648320b47b8930f978fd71341da6103a6c74a808
Marcos <marcos1000.g@hotmail.com>
2022-10-22 12:55:18 -0300

    first commit

---
+++ RelogioPy.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 22 11:32:01 2022
+
+@author: D3st0y
+"""
+
+# -*- coding: utf-8 -*-
+import tkinter as tk
+
+import datetime
+
+class TelaPrincipal:
+    def __init__(self, master):
+        self.nossaTela = master
+        self.lblRelogio = tk.Label(
+            self.nossaTela, font=('Arial', 26), fg='Black')
+        self.lblRelogio.pack(pady=30, padx=30)
+        self.alteracao()
+
+    def alteracao(self):
+        now = datetime.datetime.now()
+
+        self.lblRelogio['text'] = now.strftime('%H:%M:%S')
+
+        self.nossaTela.after(1000, self.alteracao)
+
+
+janelaRaiz = tk.Tk()
+TelaPrincipal(janelaRaiz)
+janelaRaiz.mainloop()
